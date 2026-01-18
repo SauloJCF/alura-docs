335708c61f704b834185aa07fd0d24849b91de06
Saulo.Figueiredo <saulojcf@gmail.com>
2026-01-18 11:34:24 -0300

    Persistência em memória

--- src/socket-back.js
+++ src/socket-back.js
@@ -1,15 +1,32 @@
 import io from "./servidor.js";
 
+const documentos = [
+  {
+    nome: "JavaScript",
+    texto: "Texto de JavaScript..."
+  },
+  {
+    nome: "Node",
+    texto: "Texto de Node..."
+  },
+  {
+    nome: "Socket.io",
+    texto: "Texto de Socket.io..."
+  },
+];
+
 io.on("connection", (socket) => {
   console.log("Um cliente se conectou: " + socket.id);
 
   socket.on("selecionar_documento", (nomeDocumento) => {
+    const documento = encontrarDocumento(nomeDocumento);
+
+    console.log(documento);
+
     socket.join(nomeDocumento);
   });
 
   socket.on("texto_editor", ({ texto, nomeDocumento }) => {
-    // socket.broadcast.emit("texto_editor_clientes", texto);
-
     socket.to(nomeDocumento).emit("texto_editor_clientes", texto);
   });
 
@@ -18,3 +35,11 @@ io.on("connection", (socket) => {
     Motivo: ${motivo}`);
   });
 });
+
+function encontrarDocumento(nomeDocumento) {
+  const documentoEncontrado = documentos.find((documento) => {
+    return documento.nome === nomeDocumento;
+  });
+
+  return documentoEncontrado;
+}
