95a381593c1d9ce59d45c2a0ab8bceec2ce41c34
Saulo.Figueiredo <saulojcf@gmail.com>
2026-02-01 11:12:13 -0300

    Autenticação parte 2

--- public/login/socket-front-login.js
+++ public/login/socket-front-login.js
@@ -4,4 +4,13 @@ function emitirAutenticarUsuario(dados) {
     socket.emit("autenticar_usuario", dados);
 }
 
+socket.on("autenticacao_sucesso", () => {
+    alert("Usuário autenticado com sucesso!");
+    window.location.href = "/";
+});
+
+socket.on("autenticacao_erro", () => alert("Erro ao autenticar usuário."));
+
+socket.on("usuario_nao_encontrado", () => alert("Usuário não encontrado."));
+
 export { emitirAutenticarUsuario };
--- servidor/registrarEventos/login.js
+++ servidor/registrarEventos/login.js
@@ -1,10 +1,21 @@
 import { encontrarUsuario } from "../db/usuariosdb.js";
+import autenticarUsuario from "../utils/autenticarUsuario.js";
 
 function registrarEventosLogin(socket, io) {
-    socket.on("autenticar_usuario", async ({nome, senha}) => {
+    socket.on("autenticar_usuario", async ({ nome, senha }) => {
         const usuario = await encontrarUsuario(nome);
 
-        console.log(usuario);
+        if (usuario) {
+            const autenticado = autenticarUsuario(senha, usuario);
+
+            if (autenticado) {
+                socket.emit("autenticacao_sucesso");
+            } else {
+                socket.emit("autenticacao_erro");
+            }
+        } else {
+            socket.emit("usuario_nao_encontrado");
+        }
     });
 }
 
