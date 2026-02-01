4dcff38d3c5aa53b2732e97edae70fbecd857f47
Saulo.Figueiredo <saulojcf@gmail.com>
2026-02-01 10:52:58 -0300

    Autenticação parte 1

--- servidor/socket-back.js
+++ servidor/socket-back.js
@@ -3,9 +3,11 @@ import io from "./servidor.js";
 import registrarEventosInicio from './registrarEventos/inicio.js';
 import registrarEventosDocumento from './registrarEventos/documento.js';
 import registrarEventosCadastro from './registrarEventos/cadastro.js';
+import registrarEventosLogin from './registrarEventos/login.js';
 
 io.on("connection", (socket) => {
   registrarEventosInicio(socket, io);
   registrarEventosDocumento(socket, io);
   registrarEventosCadastro(socket, io);
+  registrarEventosLogin(socket, io);
 });
