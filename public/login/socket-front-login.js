38798149c895fd0c214e11e38f9facc6f40d043c
Saulo.Figueiredo <saulojcf@gmail.com>
2026-02-07 15:19:48 -0300

    Gerenciamento cookies front end

--- public/login/socket-front-login.js
+++ public/login/socket-front-login.js
@@ -1,10 +1,14 @@
+import { definirToken } from "../utils/cookies.js";
+
 const socket = io();
 
 function emitirAutenticarUsuario(dados) {
     socket.emit("autenticar_usuario", dados);
 }
 
-socket.on("autenticacao_sucesso", () => {
+socket.on("autenticacao_sucesso", (tokenJWT) => {
+    definirToken("tokenJWT", tokenJWT);
+
     alert("Usuário autenticado com sucesso!");
     window.location.href = "/";
 });
---
+++ public/utils/cookies.js
@@ -0,0 +1,17 @@
+function definirToken(chave, valor) {
+    document.cookie = `${chave}=${valor};path=/`;
+}
+
+function obterCookie(chave) {
+    return document
+        .cookie
+        .split("; ")
+        .find((cookie) => cookie.startsWith(`${chave}=`))
+        ?.split("=")[1];
+}
+
+function removerCookie(chave) {
+    document.cookie = `${chave}=; expires=Thu, 01 Jan 1970 00:00:00`;
+}
+
+export { definirToken, obterCookie, removerCookie };
