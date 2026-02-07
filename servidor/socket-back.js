037a6fbbabe6fd274216310d0ae7ab948fa98425
Saulo.Figueiredo <saulojcf@gmail.com>
2026-02-07 16:57:37 -0300

    Middlewares pt 1

--- servidor/socket-back.js
+++ servidor/socket-back.js
@@ -4,6 +4,9 @@ import registrarEventosInicio from './registrarEventos/inicio.js';
 import registrarEventosDocumento from './registrarEventos/documento.js';
 import registrarEventosCadastro from './registrarEventos/cadastro.js';
 import registrarEventosLogin from './registrarEventos/login.js';
+import autorizarUsuario from './middlewares/autorizarUsuario.js';
+
+io.use(autorizarUsuario);
 
 io.on("connection", (socket) => {
   registrarEventosInicio(socket, io);
