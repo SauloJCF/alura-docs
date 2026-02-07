35c50a9ec38b20bdd9238c4db1784c663ad55c0d
Saulo.Figueiredo <saulojcf@gmail.com>
2026-02-07 17:16:36 -0300

    Middlewares parte 2

--- servidor/middlewares/autorizarUsuario.js
+++ servidor/middlewares/autorizarUsuario.js
@@ -1,5 +1,15 @@
-function autorizarUsuario(socket, next)  {
-    next();
+import jwt from "jsonwebtoken";
+
+function autorizarUsuario(socket, next) {
+    try {
+        const token = socket.handshake.auth.token;
+
+        jwt.verify(token, process.env.SEGREDO_JWT);
+
+        next();
+    } catch (error) {
+        next(error);
+    }
 }
 
 export default autorizarUsuario;
